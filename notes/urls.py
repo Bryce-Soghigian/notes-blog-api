ca8e53839162cb325655a0aa70a55f4185e85dd5
bryce-soghigian <bsoghigian@gmail.com>
2021-11-28 03:34:31 -0800

    reworked how notes are being returned

--- notes/urls.py
+++ notes/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('v1/notes/sorted', views.SortedEntries.as_view(), name='sorted_entries'),
+    path('v1/notes/retrieve/<note_id>', views.RetrieveEntry.as_view(), name='retrieve_entry'),
     path('v1/notes/entry', views.EntryView.as_view(), name='entry_crud'),
  ]
